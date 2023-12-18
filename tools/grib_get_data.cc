51fc080cdb4e5feca61245f0f593626659c4b6c1
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-12-18 16:28:49 +0000

    Work on eckit_geo

--- tools/grib_get_data.cc
+++ tools/grib_get_data.cc
@@ -8,81 +8,73 @@
  * virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
  */
 
+
 #include <cstdio>
-#include <string>
 #include <iostream>
-#include <vector>
 #include <map>
+#include <string>
+#include <vector>
 
-#include "grib_tools.h"
+#include "unistd.h"
 
-static void print_key_values(grib_values* values, int values_count);
-static grib_values* get_key_values(grib_runtime_options* options, grib_handle* h);
+#include "eccodes.h"
 
-
-grib_option grib_options[] = {
-    /*  {id, args, help}, on, command_line, value */
-    {"m:", "missingValue",
-     "\n\t\tThe missing value is given through this option."
-     "\n\t\tAny string is allowed and it is printed in place of the missing"
-     "\n\t\tvalues. Default is to skip the missing values.\n"},
-    {"F:", "format", "\n\t\tC style format for data values. Default is \"%.10e\"\n", 0, 1, nullptr},
-    {"L:", "format", "\n\t\tC style format for latitudes/longitudes. Default is \"%9.3f%9.3f\"\n", 0, 1, nullptr},
-    {"s:", nullptr, nullptr, 0, 1, nullptr},
-};
-
-const char* tool_description =
-    ;
-const char* tool_name       = "grib_get_data";
-const char* tool_online_doc = "https://confluence.ecmwf.int/display/ECC/grib_get_data";
-const char* tool_usage      = ;
-
-int grib_options_count = sizeof(grib_options) / sizeof(grib_option);
-
-
-
-
-
-
+#include "eckit/exception/Exceptions.h"
 
 
 int main(int argc, char* argv[]) {
     // options
-    struct option_t{
+    struct option_t {
         std::string args;
         std::string help;
         std::string value;
     };
 
-    std::map<char, option_t> options{
-                                     {'m',{"missingValue", "The missing value is given through this option. Any string is allowed and it is printed in place of the missing values. Default is to skip the missing values."}},
-                                     {'F', {"format", "C style format for data values. Default is \"%.10e\""}},
-                                     {'L', {"format", "C style format for latitudes/longitudes. Default is \"%9.3f%9.3f\""}},
-                                     {'s', {"", ""}},
-                                     };
 
-    for (int opt=0;(opt = getopt(argc, argv, "m:F:L:s:")) != -1;) {
+    auto usage = [](const std::string& tool, const std::map<char, option_t>& options) {
+        std::cout << "\nNAME \t" << tool
+                  << "\n"
+                     "\nDESCRIPTION"
+                     "\n\tPrint a latitude, longitude, data values list."
+                     "\n"
+                     "\tNote: Rotated grids are first unrotated"
+                     "\n"
+                     "\n"
+                     "USAGE "
+                     "\n\t"
+                  << tool
+                  << " [options] grib_file"  // " grib_file ..."
+                     "\n"
+                     "\n"
+                     "OPTIONS"
+                     "\n";
+        for (const auto& option : options) {
+            std::cout << "\t-" << option.first << " " << option.second.args << "\t" << option.second.help << "\n";
+        }
+        std::cout << "\n";
+    };
+
+
+    std::map<char, option_t> options{
+        {'m',
+         {"missingValue",
+          "The missing value is given through this option. Any string is allowed and it is printed in place of the "
+          "missing values. Default is to skip the missing values."}},
+        {'F', {"format", "C style format for data values. Default is \"%.10e\""}},
+        {'L', {"format", "C style format for latitudes/longitudes. Default is \"%9.3f%9.3f\""}},
+        {'s', {"", ""}},
+    };
+
+    if (argc != 2) {
+        usage(argv[0], options);
+        exit(1);
+    }
+
+    for (int opt = 0; (opt = getopt(argc, argv, "m:F:L:s:")) != -1;) {
         auto key = static_cast<char>(opt);
 
         if (key == '?' || key == 'h') {
-            std::cout << "\nNAME \tgrib_get_data"
-                         "\n"
-                         "\nDESCRIPTION"
-                         "\n\tPrint a latitude, longitude, data values list."
-                         "\n"
-                         "\tNote: Rotated grids are first unrotated"
-                         "\n"
-                         "\n"
-                         "USAGE "
-                         "\n\tgrib_get_data [options] grib_file grib_file ..."
-                         "\n"
-                         "\n"
-                         "OPTIONS"
-                         "\n";
-            for (const auto& option : options) {
-                    std::cout << "\t-" << option.first << " " << option.second.args << "\t" << option.second.help << "\n";
-            }
-            std::cout << "\n";
+            usage(argv[0], options);
             exit(1);
         };
 
@@ -90,28 +82,59 @@ int main(int argc, char* argv[]) {
     }
 
 
+    int err            = 0;
+    size_t bmp_len     = 0;
+    size_t values_len  = 0;
+    long bitmapPresent = 0;
 
 
+    auto* in = std::fopen(argv[1], "r");
+    ASSERT_MSG(in != nullptr, "ERROR: unable to open file '" + std::string(argv[1]) + "'");
 
+    for (codes_handle* h = nullptr; (h = codes_handle_new_from_file(nullptr, in, PRODUCT_GRIB, &err)) != nullptr;) {
+        ASSERT(err == CODES_SUCCESS);
 
+        // values
+        CODES_CHECK(codes_get_size(h, "values", &values_len), nullptr);
+        std::vector<double> values(values_len);
+        CODES_CHECK(codes_get_double_array(h, "values", values.data(), &values_len), nullptr);
 
+        // bitmap
+        std::vector<long> bitmap;
 
+        CODES_CHECK(codes_get_long(h, "bitmapPresent", &bitmapPresent), nullptr);
+        if (bitmapPresent) {
+            CODES_CHECK(codes_get_size(h, "bitmap", &bmp_len), nullptr);
+            ASSERT(values_len == bmp_len);
+            bitmap.resize(bmp_len);
+            CODES_CHECK(codes_get_long_array(h, "bitmap", bitmap.data(), &bmp_len), nullptr);
+            printf("Bitmap is present. Num = %lu\n", bmp_len);
+        }
 
-}
+        // lat/lon/values iterator
+        auto* iter = codes_grib_iterator_new(h, 0, &err);
+        ASSERT(err == CODES_SUCCESS);
 
-int grib_tool_new_filename_action(grib_runtime_options* options, const char* file) {
+        size_t n = 0;
+        for (double lat = 0, lon = 0, value = 0; codes_grib_iterator_next(iter, &lat, &lon, &value) != 0; ++n) {
+            auto is_missing_val = bitmapPresent && bitmap[n] == 0;
+            if (!is_missing_val) {
+                printf("- %d - lat=%f lon=%f value=%f\n", static_cast<int>(n), lat, lon, value);
+            }
+        }
+        ASSERT(n == values_len);
+
+        codes_grib_iterator_delete(iter);
+        codes_handle_delete(h);
+    }
+
+    std::fclose(in);
     return 0;
 }
 
-int grib_tool_new_file_action(grib_runtime_options* options, grib_tools_file* file) {
-    exit_if_input_is_directory(tool_name, file->name);
-    return 0;
-}
 
+#if 0
 int grib_tool_new_handle_action(grib_runtime_options* options, grib_handle* h) {
-
-
-
     int err = 0;
 
     long numberOfPoints = 0;
@@ -119,21 +142,11 @@ int grib_tool_new_handle_action(grib_runtime_options* options, grib_handle* h) {
     grib_values* values = nullptr;
     grib_iterator* iter = nullptr;
 
-    bool print_keys   = grib_options_on("p:") != 0;
     bool skip_missing = true;
 
     std::string missing_string;
     std::string format_values  = "%.10e";
     std::string format_latlons = "%9.3f%9.3f";
-
-    if (options->skip == 0) {
-        if (options->set_values_count != 0) {
-            err = grib_set_values(h, options->set_values, options->set_values_count);
-        }
-        if (err != GRIB_SUCCESS && options->fail != 0) {
-            exit(err);
-        }
-    }
 
     if (grib_options_on("m:") != 0) {
         /* User wants to see missing values */
@@ -220,9 +233,6 @@ int grib_tool_new_handle_action(grib_runtime_options* options, grib_handle* h) {
         exit(err);
     }
 
-    /* Cater for GRIBs which have missing values but no bitmap */
-    /* See ECC-511 */
-
     bool hasMissingValues = false;
     {
         long value = 0;
@@ -252,17 +262,7 @@ int grib_tool_new_handle_action(grib_runtime_options* options, grib_handle* h) {
 
     std::fprintf(dump_file, "Value");
 
-    if (print_keys) {
-        for (int i = 0; i < options->print_keys_count; i++) {
-            std::fprintf(dump_file, " %s", options->print_keys[i].name);
-        }
-    }
-
     std::fprintf(dump_file, "\n");
-
-    if (print_keys) {
-        values = get_key_values(options, h);
-    }
 
     for (int i = 0; i < numberOfPoints; i++) {
         bool is_missing_val = hasMissingValues && bitmapPresent ? (bitmap[i] == 0) : (data_values[i] == missingValue);
@@ -278,9 +278,6 @@ int grib_tool_new_handle_action(grib_runtime_options* options, grib_handle* h) {
                 std::fprintf(dump_file, format_values.c_str(), data_values[i]);
             }
 
-            if (print_keys) {
-                print_key_values(values, options->print_keys_count);
-            }
             std::fprintf(dump_file, "\n");
         }
     }
@@ -291,81 +288,4 @@ int grib_tool_new_handle_action(grib_runtime_options* options, grib_handle* h) {
 
     return 0;
 }
-
-int grib_tool_skip_handle(grib_runtime_options*, grib_handle* h) {
-    grib_handle_delete(h);
-    return 0;
-}
-
-void grib_tool_print_key_values(grib_runtime_options* options, grib_handle* h) {
-    grib_print_key_values(options, h);
-}
-
-int grib_tool_finalise_action(grib_runtime_options*) {
-    return 0;
-}
-
-static void print_key_values(grib_values* values, int values_count) {
-    for (int i = 0; i < values_count; i++) {
-        std::fprintf(dump_file, " %s", values[i].string_value);
-    }
-}
-
-static grib_values* get_key_values(grib_runtime_options* options, grib_handle* h) {
-    int ret    = 0;
-    size_t len = MAX_STRING_LEN;
-    char str[len];
-    std::string value;
-
-    for (int i = 0; i < options->print_keys_count; i++) {
-        ret = GRIB_SUCCESS;
-
-        if (grib_is_missing(h, options->print_keys[i].name, &ret) != 0 && ret == GRIB_SUCCESS) {
-            options->print_keys[i].type = GRIB_TYPE_MISSING;
-            value                       = "MISSING";
-        }
-        else if (ret != GRIB_NOT_FOUND) {
-            if (options->print_keys[i].type == GRIB_TYPE_UNDEFINED) {
-                grib_get_native_type(h, options->print_keys[i].name, &(options->print_keys[i].type));
-            }
-
-            switch (options->print_keys[i].type) {
-                case GRIB_TYPE_STRING:
-                    ret   = grib_get_string(h, options->print_keys[i].name, str, &len);
-                    value = str;
-                    break;
-                case GRIB_TYPE_DOUBLE:
-                    ret   = grib_get_double(h, options->print_keys[i].name, &(options->print_keys[i].double_value));
-                    value = std::to_string(options->print_keys[i].double_value);
-                    break;
-                case GRIB_TYPE_LONG:
-                    ret   = grib_get_long(h, options->print_keys[i].name, &(options->print_keys[i].long_value));
-                    value = std::to_string(options->print_keys[i].long_value);
-                    break;
-                default:
-                    std::fprintf(dump_file, "invalid type for %s\n", options->print_keys[i].name);
-                    exit(1);
-            }
-        }
-
-        if (ret != GRIB_SUCCESS) {
-            if (options->fail != 0) {
-                GRIB_CHECK_NOLINE(ret, options->print_keys[i].name);
-            }
-            if (ret == GRIB_NOT_FOUND) {
-                value = "not found";
-            }
-            else {
-                std::fprintf(dump_file, "%s %s\n", grib_get_error_message(ret), options->print_keys[i].name);
-                exit(ret);
-            }
-        }
-        options->print_keys[i].string_value = strdup(value.c_str());
-    }
-    return options->print_keys;
-}
-
-int grib_no_handle_action(grib_runtime_options* options, int err) {
-    std::fprintf(dump_file, "\t\t\"ERROR: unreadable message\"\n");
-    return 0;
-}
+#endif
